a124ba1be1f9b217d9c42c03f12264318237fe83
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2025-07-28 20:44:08 +0530

    Add a commponents

--- src/app/accord/page.tsx
+++ src/app/accord/page.tsx
@@ -5,21 +5,40 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 
 export default function AccordPage() {
-
   return (
-   <div className="flex justify-center items-center ">
-     <main className="w-[500px] p-4">
-       <Accordion type="single" collapsible>
-         <AccordionItem value="item-1">
-           <AccordionTrigger>Is it accessible?</AccordionTrigger>
-           <AccordionContent>
-             Yes. It adheres to the WAI-ARIA design pattern.
-           </AccordionContent>
-         </AccordionItem>
-       </Accordion>
-     </main>
-   </div>
+    <div className="flex justify-center items-center ">
+      <main className="w-[500px] p-4">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </main>
+
+      <InputOTP maxLength={6}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+    </div>
   );
 }
